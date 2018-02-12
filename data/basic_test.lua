177bd33402ad141b0f1f1134865faa4aa7e14fa9
Maxence Reberol <maxence.reberol@gmail.com>
2018-02-12 17:35:19 +0100

    Add basic Lua script

---
+++ data/basic_test.lua
@@ -0,0 +1,1 @@
+scene_graph.current().query_interface("OGF::MeshGrobFemCommands").fem({sourceterm_value="1", dirichlet_region="1", neumann_value="0", solution_name="u", diffusion_value="1", dirichlet_value="0", neumann_region="0"})
